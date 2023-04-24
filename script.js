01b8287361fd5c66160ec6a3d0929f534ad2e9d0
dylanwheatley <dylan.r.wheatley@gmail.com>
2023-04-24 09:58:47 -0400

    Added full grid in flexbox, added mousover function and reset button. Need to add custom grid size next.

--- script.js
+++ script.js
@@ -7,14 +7,41 @@ let divGrid;
 
 let container = document.querySelector('#container');
 
+
+
 function divMaker (gridSize) {
 
-        for (i=0; i<gridSize; i++) {
+        //process to make all of the grids
+        for (i=0; i<gridSize*gridSize; i++) {
             divGrid = document.createElement('div');
             divGrid.classList.add('gridStyle');
             container.appendChild(divGrid);
             console.log("test " + i);
         }
+
+
+
+        //adding the black hover effect to 'draw'
+        let etch = document.querySelectorAll('.gridStyle');
+
+        etch.forEach((etch) => {
+            etch.addEventListener('mouseover', () => {
+                etch.classList.add('hover');  
+            }); 
+        });
+
+
+        
+        //the reset button to erase the board
+        let reset = document.querySelector('button');
+            reset.addEventListener('click', () => {
+                etch.forEach((etch) => {
+                 etch.classList.remove('hover');
+        });
+    });
 }
+
+
+
 
 divMaker(16);
